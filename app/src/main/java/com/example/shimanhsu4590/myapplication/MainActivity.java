dad8536b2f8bd4f7463247b80b922960212e357c
BHASKAR TOMAR <bhaskartomar04@gmail.com>
2016-06-18 19:52:04 +0530

    Login problem solved.

--- app/src/main/java/com/example/shimanhsu4590/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shimanhsu4590/myapplication/MainActivity.java
@@ -1,57 +1,55 @@
 package com.example.shimanhsu4590.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper helper = new DatabaseHelper(this);
+    DatabaseHelper helper;
+    EditText a;
+    EditText b;
+    String str;
+    String pass;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        }
+        helper = new DatabaseHelper(this);
+        a = (EditText) findViewById(R.id.TFusername);
+        b = (EditText) findViewById(R.id.TFpassword);
+
+    }
 
     public void onButtonClick(View v) {
 
-        if(v.getId() == R.id.Blogin)
-        {
-        EditText a = (EditText)findViewById(R.id.TFusername);
-        String str = a.getText().toString();
-
-        EditText b = (EditText)findViewById(R.id.TFpassword);
-        String pass = b.getText().toString();
-        String password =  helper.searchPass(str);
-            if (pass.equals(password))
-            {
-                Intent i = new Intent(MainActivity.this, Section.class);
-                i.putExtra("Username",str);
+        switch (v.getId()) {
+            case R.id.Blogin:
+                str = a.getText().toString();
+                pass = b.getText().toString();
+                password = helper.searchPass(str);
+                if (pass.equals(password)) {
+                    Intent i = new Intent(MainActivity.this, Section.class);
+                    i.putExtra("Username", str);
+                    startActivity(i);
+                } else {
+                    Toast temp = Toast.makeText(MainActivity.this, "username and passwords don't match", Toast.LENGTH_SHORT);
+                    temp.show();
+                }
+                break;
+            case R.id.Bsignup:
+                Intent i = new Intent(MainActivity.this, signup.class);
                 startActivity(i);
-
-            }
-            else
-            {
-                Toast temp= Toast.makeText(MainActivity.this ,"username and passwords don't match" , Toast.LENGTH_SHORT);
-                temp.show();
-            }
-
-
-        Intent i = new Intent(MainActivity.this, Section.class);
-        i.putExtra("Username",str);
-        startActivity(i);}
-        if(v.getId() == R.id.Bsignup)
-        {
-            Intent i = new Intent(MainActivity.this, signup.class);
-            startActivity(i);
+                break;
 
         }
+
     }
 }
 
